0262dc45de1550082d453d1fa03e1c1898f6207e
Daniel <xt1zer@outlook.com>
2020-02-12 11:58:03 +0300

    Test fucntionality

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
 #include "Stack.h"
-//#include <iostream>
-//using std::printf;
+#include <iostream>
+using std::printf;
 
 int main() {
-    Stack<int> s;
-    s.pop();
+	Stack<int> s;
+	s.push(2);
+	s.push(5);
+	bool empty = s.isEmpty();
+	int top = s.peek();
+	printf("%d %d\n", empty, top);
 
-    return 0;
+	s.clear();
+	empty = s.isEmpty();
+	top = s.peek();
+	printf("%d %d\n", empty, top);
+
+	//int pop = s.pop();
+	//empty = s.isEmpty();
+	//top = s.peek();
+	//printf("%d %d %d\n", pop, empty, top);
+
+	//pop = s.pop();
+	//empty = s.isEmpty();
+	//top = s.peek();
+	//printf("%d %d %d\n", pop, empty, top);
+
+	return 0;
 }
