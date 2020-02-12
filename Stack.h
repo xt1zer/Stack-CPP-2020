4047f901188a9576be12f940cae03bd75d2203d1
Daniel <xt1zer@outlook.com>
2020-02-12 11:57:43 +0300

    Add Stack methods

--- Stack.h
+++ Stack.h
@@ -1,23 +1,72 @@
-﻿#pragma once
+#pragma once
+#include <limits>
+#include <fstream>
+using std::ofstream;
 
 template <typename T>
 struct Node {
-    T value;
-    Node* next;
+	T value;
+	Node* next;
+
+	Node(T data) : value(data), next(nullptr){}
+
+	~Node(){
+		next = nullptr;
+	}
 };
 
 template <typename T>
 class Stack {
 private:
-    Node<T>* top;
-public:
-    typedef Node<T>* Iterator;
+	Node<T>* top = nullptr;
 
-    void push(T value);
-    T pop();
-    T peek();
-    void clear();
-    bool isEmpty();
-    void save(char* fileName);
-    void load(char* fileName);
+public:
+	typedef Node<T>* Iterator;
+
+	void push(const T& data) {
+		Node<T>* newNode = new Node<T>(data);
+		if (!isEmpty())
+			newNode->next = top;
+		top = newNode;
+	};
+
+	T pop() {
+		if (isEmpty())
+			return INT_MIN;
+
+		T output = top->value;
+		Node<T>* oldTop = top;
+
+		if (top->next)
+			top = top->next;
+		else
+			top = nullptr;
+
+		delete oldTop;
+
+		return output;
+	};
+
+	T peek(){
+		if (!isEmpty())
+			return top->value;
+		return INT_MIN;
+	}
+
+	void clear() {
+		while (!isEmpty())
+			pop();
+	};
+
+	bool isEmpty() {
+		return !top;
+	};
+
+	void save(const char* fileName){
+		ofstream outFile(fileName);
+	};
+
+	void load(const char* fileName){
+
+	};
 };
