26b8a593b94220225acd96c6c6a1f1cd98bd6085
Daniel White <xt1zer@outlook.com>
2020-03-01 23:56:17 +0300

    Add initialise test, fill test, clear test, push and pop test

--- test/stackTest.cpp
+++ test/stackTest.cpp
@@ -4,6 +4,7 @@
 class StackTest: public testing::Test {
 	public:
 		Stack<int>* s;
+		int size = 0;
 
 		void SetUp() {
 		}
@@ -14,5 +15,29 @@ class StackTest: public testing::Test {
 
 TEST_F(StackTest, InitStack) {
 	s = new Stack<int>;
-	ASSERT_TRUE(s->top() == nullptr);
+	ASSERT_TRUE(!s->top());
+}
+
+TEST_F(StackTest, FillStack) {
+	for (int i = 0; i < 50; ++i) {
+		s->push(i);
+		++size;
+	}
+	EXPECT_EQ(s->peek(), 49);
+	ASSERT_EQ(size, 50);
+}
+
+TEST_F(StackTest, ClearStack) {
+	s->clear();
+	size = 0;
+	ASSERT_EQ(s->isEmpty(), true);
+}
+
+TEST_F(StackTest, PushAndPop) {
+	s->push(size++);
+	int popped = s->pop();
+	size = 0;
+
+	ASSERT_EQ(popped, 0);
+	ASSERT_EQ(s->pop(), INT32_MIN);
 }
